0e91e1c4b0713d68dc9a20f5e2e68df852061653
jeffeloy <contatojeloydev@gmail.com>
2025-04-28 12:36:27 -0300

    feat: add client configuration data

---
+++ src/config/clientConfig.ts
@@ -0,0 +1,16 @@
+export const clients = [
+  {
+    id: "cma164rx3000b0cl43y2a65uf",
+    name: "Cliente Macapá",
+    dbType: "mysql",
+    isFormatNameToUpperCase: true,
+    isFormatPhone: true,
+  },
+  {
+    id: "cma168zec000c0cl4hy6yhlih",
+    name: "Cliente Varejão",
+    dbType: "mongodb",
+    isFormatNameToUpperCase: false,
+    isFormatPhone: false,
+  },
+];
